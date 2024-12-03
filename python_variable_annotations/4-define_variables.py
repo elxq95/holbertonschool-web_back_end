9ba36fe88433700ef87cadc65a0a40262df382f2
elxq95 <leongxiqern12@gmail.com>
2024-12-03 17:07:16 +1100

    add 4 and 5

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,14 @@
+"""
+This module defines and annotates several variables with specific types and values.
+
+Variables:
+- a: An integer with a value of 1.
+- pi: A float with a value of 3.14.
+- i_understand_annotations: A boolean with a value of True.
+- school: A string with a value of "Holberton".
+"""
+
+a: int = 1  # Integer variable representing a basic numeric value.
+pi: float = 3.14  # Float variable representing the mathematical constant Pi.
+i_understand_annotations: bool = True  # Boolean variable indicating understanding of annotations.
+school: str = "Holberton"  # String variable representing the name of the school.
---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,13 @@
+from typing import List
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Takes a list of floats and returns their sum as a float.
+    
+    Args:
+        input_list (List[float]): A list of float numbers.
+    
+    Returns:
+        float: The sum of the float numbers in the list.
+    """
+    return sum(input_list)
