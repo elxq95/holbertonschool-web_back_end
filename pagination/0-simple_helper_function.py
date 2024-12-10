544bd7de10a32a971a87a34fba3d5e54c057186e
elxq95 <leongxiqern12@gmail.com>
2024-12-10 15:22:47 +1100

    push

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,16 @@
+#!/bin/usr/env python3
+
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    Function that calculates the start and end index for pagination.
+
+    Args:
+        page (int): The current page number (1-indexed).
+        page_size (int): The number of items per page.
+
+    Returns:
+        tuple: A tuple containing the start index and end index.
+    """
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return start_index, end_index
