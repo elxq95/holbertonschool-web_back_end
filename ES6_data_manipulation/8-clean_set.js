83128d3a665d39b378cc46a8dd140a7cc0edfba3
elxq95 <leongxiqern12@gmail.com>
2024-12-17 09:42:34 +1100

    task 6-10

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,16 @@
+export default function cleanSet(set, startString = '') {
+    const modifiedArray = [];
+  
+    if (!startString || typeof startString !== 'string') {
+      return '';
+    }
+  
+    for (const value of set) {
+      if (value.startsWith(startString)) {
+        const newValue = value.slice(startString.length);
+        modifiedArray.push(newValue);
+      }
+    }
+    return modifiedArray.join('-');
+  }
+  
